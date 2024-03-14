251fbcbf3ca9498e246becaab72f794bf59e091d
Nazlı Anapa <114147817+ichbinazli@users.noreply.github.com>
2024-03-14 12:22:09 +0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -45,22 +45,21 @@ static int	have_nl(char *s)
 
 static char	*extract_line(char *stash)
 {
-	char	*line;							//Satırın kopyalanacağı dizi
-	size_t	i;
+	char	*line;
 	size_t	j;
 
 	i = 0;
 	if (!stash)
 		return (0);
-	while (stash[i] != '\n')				//Yeni satır karakterinin konumunu bulmak için
+	while (stash[i] != '\n')
 		i++;
-	line = malloc(sizeof(char) * (i + 2));	//Satırı kopyalamak için bellek tahsis edilir. '\n' karakteri de kopyalanacağı için +2 eklenir.
+	line = malloc(sizeof(char) * (i + 2));
 	if (!line)
 		return (0);
 	j = 0;
 	while (j < i + 1)
 	{
-		line[j] = stash[j];					// Satır, stash'ten line'a kopyalanır.
+		line[j] = stash[j];
 		j++;
 	}
 	line[j] = '\0';
@@ -70,35 +69,35 @@ static char	*extract_line(char *stash)
 static char	*recreate_stash(char *stash)
 {
 	size_t	i;
-	char	*res;							//Yeniden oluşturulmuş stash'i tutacak bir karakter dizisi
+	char	*res;
 
 	i = 0;
 	if (!stash)
 		return (NULL);
-	while (stash[i] != '\n')				//Yeniden oluşturma işlemi için yeni satır karakterinin konumunu bulmak için
+	while (stash[i] != '\n')
 		i++;
-	if (stash[i + 1] == '\0')				//Yeniden oluşturma sonunda, satır sonuna geldiyse NULL döndürür.
+	if (stash[i + 1] == '\0')
 		return (free_stash(&stash, 0));
-	res = ft_substr(stash, i + 1, ft_strlen(stash));//Yeni stash, orijinal stash'ten satır sonrasını kopyalayarak oluşturulur.
-	if (!res)								//Tahsis başarısızsa mevcut stash bellekten serbest bırakılır ve NULL döndürülür.
+	res = ft_substr(stash, i + 1, ft_strlen(stash));
+	if (!res)
 	{
 		free_stash(&stash, 0);
 		return (NULL);
 	}
-	free_stash(&stash, 0);					// Yeniden oluşturma tamamsa orijinal stash bellekten serbest bırakılır
-	return (res); 							//ve yeniden oluşturulmuş stash döndürülür.
+	free_stash(&stash, 0);
+	return (res);
 }
 
 char	*get_next_line(int fd)
 {
-	char		buf[BUFFER_SIZE + 1];		//Bellekte okunan veriyi geçici olarak tutacak
-	long		bytes_read;					//Okunan bayt sayısını takip etmek için
-	static char	*stash = NULL;				//Önceki çağrılardan kalan veriyi saklamak için
-	char		*line;						//Okunan satırın saklanacağı karakter dizisi
+	char		buf[BUFFER_SIZE + 1];
+	long		bytes_read;
+	static char	*stash = NULL;
+	char		*line;
 
 	line = 0;
 	bytes_read = BUFFER_SIZE;
-	if (fd < 0 || BUFFER_SIZE <= 0)			//Dosya tanımlayıcısının geçerliliği ve bellek boyutunun pozitif olup olmadığı kontrol edilir.
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (free_stash(&stash, 0));
 	while (bytes_read > 0)
 	{
@@ -106,13 +105,13 @@ char	*get_next_line(int fd)
 		if ((bytes_read <= 0 && !stash) || bytes_read == -1)
 			return (free_stash(&stash, 0));
 		buf[bytes_read] = '\0';
-		stash = copy_to_stash(stash, buf);	//Okunan veri stash alanına kopyalanır.
-		if (have_nl(stash))					//Stash'te yeni satır karakteri var mı diye kontrol edilir.
+		stash = copy_to_stash(stash, buf);
+		if (have_nl(stash))
 		{
-			line = extract_line(stash);		//Yeni satır karakteri bulunduğunda, stash'ten bir satır çıkarılır.
+			line = extract_line(stash);
 			if (!line)
-				return (free_stash(&stash, 0));	//Satır çıkarılamazsa bellek serbest bırakılır ve işlev sonlandırılır.
-			return (stash = recreate_stash(stash), line);	//Satır başarıyla çıkarıldıysa, kalan veri stash'te kalacak şekilde güncellenir ve satır döndürülür.
+				return (free_stash(&stash, 0));
+			return (stash = recreate_stash(stash), line);
 		}
 	}
 	return (free_stash(&stash, 1));
